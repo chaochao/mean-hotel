81bdb6356ced1126cd7faf921d502a997ecef294
Chao Liu <liuchao8989@gmail.com>
2016-11-27 18:22:48 -0800

    try mongoose

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,12 +1,31 @@
-var hotelData = require('../data/hotel-data.json');
+var hotelJsonData = require('../data/hotel-data.json');
+var mongoose = require("mongoose");
 
+var Schema = mongoose.Schema;
+// var ObjectId = Schema.ObjectId;
+// for only read you don't need the schema definition
+//but if you want to insert you must input all field
+//OR YOU WILL NOT BE ABLE TO CREATE DATA
+var hotelSchema = new Schema({
+  name: String
+  //... much more but only for test
+})
+var hotel = mongoose.model("hoteldbData",hotelSchema,'hotelData');
 module.exports.hotelsGetAll = function(req, res) {
   console.log(req.query);
   var count = req.query.count || 10;
   var offset = req.query.offset || 0;
   var end = offset + count;
-  var result = hotelData.slice(offset,end);
+  var result = hotelJsonData.slice(offset,end);
 
+  hotel.findById("583918740235137c4476b97f",function(err,data){console.log("test"+data)});
+  // hotel.create({name:"test"}, function(err,data){
+  //   if(err){
+  //     console.log("wrong"+err);
+  //   }else{
+  //     console.log(data);
+  //   }
+  // });
   res
     .status(200)
     .json(result);
@@ -16,7 +35,7 @@ module.exports.hotelsGetOne = function(req, res) {
   
   var hotelId = req.params.id;
 
-  var oneHotel = hotelData[hotelId]
+  var oneHotel = hotelJsonData[hotelId]
   res
     .status(200)
     .json(oneHotel);
@@ -27,7 +46,7 @@ module.exports.hotelsAddOne = function(req, res) {
   var hotelId = req.params.id;
   console.log(req.body);
 
-  var oneHotel = hotelData[hotelId]
+  var oneHotel = hotelJsonData[hotelId]
   res
     .status(200)
     .json(req.body);
